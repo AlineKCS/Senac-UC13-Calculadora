239f6d044a2966b0ce49d2714200c5291c10acef
AlineKCS <alinekercher@yahoo.com>
2022-07-24 01:04:50 -0300

    Ateração do esquema de cores

--- App.js
+++ App.js
@@ -4,15 +4,13 @@ import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 
 export default function App() {
   // Mapeamento de teclas
-  const buttons = ['LIMPAR', 'DEL', '%', '/', 7, 8, 9, "*", 4, 5, 6,'-', 1, 2, 3 , '+', 0, '.', '+/-', '='] 
+  const buttons = ['LIMPAR', 'DEL', '%', '/', 7, 8, 9, "*", 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '+/-', '=']
 
   const [currentNumber, setCurrentNumber] = useState("")
   const [lastNumber, setLastNumber] = useState("")
 
-  function calculator(){
-      
-        
-      setCurrentNumber((eval(currentNumber)).toString())
+  function calculator() {
+    setCurrentNumber((eval(currentNumber)).toString())
   }
 
   function handleInput(buttonPressed) {
@@ -39,12 +37,12 @@ export default function App() {
       case '+/-':
         setCurrentNumber((lastNum * - 1).toString())
         return
-      case '%':       
-        setLastNumber(currentNumber + "%" + " = ")       
-        numbers[numbers.length-1] = lastNum / 100 
+      case '%':
+        setLastNumber(currentNumber + "%" + " = ")
+        numbers[numbers.length - 1] = lastNum / 100
         setCurrentNumber((numbers.join(" ")).toString())
-        return 
-   }
+        return
+    }
 
     setCurrentNumber(currentNumber + buttonPressed)
   }
@@ -64,12 +62,12 @@ export default function App() {
 
         {buttons.map((button) =>
           button === '=' ? // Mapeamento do botão =
-            <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, { backgroundColor: '#3dd0e3' }]}>
+            <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, { backgroundColor: '#1E1240' }]}>
               <Text style={[styles.textButton, { color: "white", fontSize: 30 }]}>{button}</Text>
             </TouchableOpacity>
             : // Mapeamento dos outros botões
             <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
-              <Text style={[styles.textButton, { color: typeof (button) === 'number' ? 'black' : '#0093a6' }]}>{button}</Text>
+              <Text style={[styles.textButton, { color: typeof (button) === 'number' ? 'white' : '#808080' }]}>{button}</Text>
             </TouchableOpacity>
         )}
       </View>
@@ -85,10 +83,10 @@ const styles = StyleSheet.create({
   results: {
     flex: 2,
     justifyContent: "center",
-    backgroundColor: "#f5f5f5"
+    backgroundColor: "#1E1240"
   },
   resultText: {
-    color: "#282F38",
+    color: "white",
     fontSize: 32,
     fontWeight: "bold",
     padding: 12,
@@ -105,7 +103,7 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
   button: {
-    backgroundColor: 'white',
+    backgroundColor: '#3D0075',
     alignItems: 'center',
     justifyContent: 'center',
     minWidth: 90,
